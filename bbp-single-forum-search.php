48d81436d9ff9e5f8bd958347ca90bf3290408bc
r-a-y <r-a-y@gmx.com>
2015-11-06 11:20:25 -0800

    Fix wrong CSS selector.

--- bbp-single-forum-search.php
+++ bbp-single-forum-search.php
@@ -125,7 +125,7 @@ class bbP_Single_Forum_Search {
 			.bbp-pagination {width:auto; line-height:2.5;}
 
 			<?php if ( function_exists( 'buddypress' ) && bp_is_action_variable( bbP_BP_Single_Forum_Search::$slug ) ) : ?>
-				body.group-bbpress-forum .bbp-pagination {clear:both; width:100%;}
+				body.groups.forum .bbp-pagination {clear:both; width:100%;}
 			<?php endif; ?>
 		</style>
 
